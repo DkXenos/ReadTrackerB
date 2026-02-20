c289ae8dd4e374091583ac24727954d677a16c84
DkXenos <superjason101010@gmail.com>
2026-02-20 14:57:29 +0700

    upload file

---
+++ ReadTrackerB/ReadTrackerBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadTrackerBApp.swift
+//  ReadTrackerB
+//
+//  Created by Jason TIo on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ReadTrackerBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
